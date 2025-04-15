5194d89ac36fb16a86d6e1dbccaf33a075773c1c
TilliRS <pptry@mail.de>
2025-04-16 01:39:39 +0200

    test pipeline for multi qubit pulse CNOT passed

--- tests/cnot.py
+++ tests/cnot.py
@@ -2,17 +2,20 @@ import numpy as np
 import pennylane as qml
 
 from pulse.pulse_gates import *
+from tests.helpers import possible_init_states
+from tests.pipeline import generate_wire_pairs
 from utils.definitions import *
 from utils.helpers import prints
 
+# PASSED ALL BELLSTATE TESTS AS WELL
 
-class PennyCircuit:
+class PennyCircuitCNOT:
 
     def __init__(self, num_qubits):
 
         self.num_qubits = num_qubits
 
-    def run_quick_circuit(self, wires, init_state=None):
+    def run_quick_circuit(self, wire_pairs, init_state=None):
         dev = qml.device('default.qubit', wires=self.num_qubits)
 
         @qml.qnode(dev)
@@ -20,40 +23,45 @@ class PennyCircuit:
             if init_state is not None:
                 qml.StatePrep(init_state, wires=range(self.num_qubits))
 
-            # qml.Hadamard(1)
-            # qml.CZ([0, 1])
-            # qml.Hadamard(1)
-            qml.CNOT(wires=wires)  # big endian
+            for wire_pair in wire_pairs:
+                if 0 <= wire_pair[0] < self.num_qubits:
+                    qml.CNOT(wires=wire_pair)
+                else:
+                    print(f"Warning: Target qubit {wire_pair[0]} is out of range (0 to {self.num_qubits - 1}).")
 
             return qml.state()
 
+        print(qml.draw(general_circuit)())
         return general_circuit()
 
 
-num_q = 2
-init = PHI_PLUS_NO_CNOT
+test_cases = generate_wire_pairs(20, 7)
 
-# PASSED TEST FOR ALL BELL STATES
-BELL_STATE_NO_CNOT = PSI_PLUS_NO_CNOT
-wires = [0, 1]
+for i, (num_qubits, wire_pairs) in enumerate(test_cases):
 
-c = PennyCircuit(num_q)
-penny_state = c.run_quick_circuit(wires, init.data)
-prints(penny_state)
+    print(f"Test Case {i + 1}:")
+    print(f"  Number of qubits (n): {num_qubits}")
+    print(f"  Wire pairs: {wire_pairs} \n")
 
+    c = PennyCircuitCNOT(num_qubits)
+    for init_function in possible_init_states:
+        init = init_function(num_qubits)
 
-print("-"*20)
-# prints(PSI_MINUS)
+        penny_state = c.run_quick_circuit(wire_pairs, init_state=init.data)
+        prints(penny_state)
 
+        current_state = [0]
+        current_state[-1] = init
+        for wire_pair in wire_pairs:
+            _, _, current_state = cnot(current_state[-1], wire_pair)
 
-_, _, states = cnot(init, wires)
+        result_state = current_state[-1]
+        prints(result_state)
 
-final_state = states[-1]
-
-prints(final_state)
-sim = statevector_similarity(penny_state, final_state)
-fid = statevector_fidelity(penny_state, final_state)
-print(f"sim = {sim}, fid = {fid}")
+        sim = statevector_similarity(penny_state, result_state)
+        fid = statevector_fidelity(penny_state, result_state)
+        print(f"sim = {sim}, fid = {fid}")
+        print(20 * "-", "\n")
 
 
 # ps = typical_phases()
