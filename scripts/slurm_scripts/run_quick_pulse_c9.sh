5a7547b77698ea3edc0db63a70562d6830d1346a
TilliRS <pptry@mail.de>
2025-05-08 23:55:13 +0200

    Reduced memory req

--- scripts/slurm_scripts/run_quick_pulse_c9.sh
+++ scripts/slurm_scripts/run_quick_pulse_c9.sh
@@ -4,7 +4,7 @@
 #SBATCH --time=01:30:00
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=64
-#SBATCH --mem=1000000MB
+#SBATCH --mem=350000MB
 #SBATCH --output="logs/slurm-%j-%x.out"
 
 module load compiler/llvm
