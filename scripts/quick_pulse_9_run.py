7e68217aa8f47e2849618d247695f68be916f4b5
TilliRS <pptry@mail.de>
2025-05-08 16:51:29 +0200

    Quick pulse 9 run

--- scripts/quick_pulse_9_run.py
+++ scripts/quick_pulse_9_run.py
@@ -21,7 +21,7 @@ stop = interval_length
 points = excluding_discrete_points + 1
 
 # Samples
-num_samples = 5000
+num_samples = 50
 
 # Hyper parameter
 num_qubits = 4  # scale
---
+++ scripts/slurm_scripts/run_quick_pulse_c9.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH --job-name=pulse_c9
+#SBATCH --partition=cpu
+#SBATCH --time=24:00:00
+#SBATCH --ntasks=12
+#SBATCH --mem=32000MB
+#SBATCH --output="logs/slurm-%j-%x.out"
+
+module load compiler/llvm
+
+module load devel/python/3.11.7
+
+source /pfs/data6/home/ka/ka_scc/ka_tc6850/pulse-fourier/.venv/bin/activate
+
+python /pfs/data6/home/ka/ka_scc/ka_tc6850/pulse-fourier/scripts/quick_pulse_9_run.py
+
