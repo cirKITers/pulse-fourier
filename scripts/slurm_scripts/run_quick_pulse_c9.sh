7e30fbad271977ef1ac730a36c062477036d3260
TilliRS <pptry@mail.de>
2025-05-08 22:30:29 +0200

    Name updated parallel script test

--- scripts/slurm_scripts/run_quick_pulse_c9.sh
+++ scripts/slurm_scripts/run_quick_pulse_c9.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --job-name=test_pulse_c9
+#SBATCH --job-name=test_pulse_c9_parallel
 #SBATCH --partition=cpu
 #SBATCH --time=00:30:00
 #SBATCH --ntasks=1
@@ -13,5 +13,5 @@ module load devel/python/3.11.7
 
 source /pfs/data6/home/ka/ka_scc/ka_tc6850/pulse-fourier/.venv/bin/activate
 
-python /pfs/data6/home/ka/ka_scc/ka_tc6850/pulse-fourier/scripts/parallel_pulse_9_run.py
+python /pfs/data6/home/ka/ka_scc/ka_tc6850/pulse-fourier/scripts/parallel_pulse_9.py
 
