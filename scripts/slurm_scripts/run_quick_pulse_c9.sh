d33779fcb6224476a4ca232c733744859aaf13cb
TilliRS <pptry@mail.de>
2025-05-09 00:54:00 +0200

    Reduce mem

--- scripts/slurm_scripts/run_quick_pulse_c9.sh
+++ scripts/slurm_scripts/run_quick_pulse_c9.sh
@@ -4,7 +4,7 @@
 #SBATCH --time=00:30:00
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=64
-#SBATCH --mem=350000MB
+#SBATCH --mem=150000MB
 #SBATCH --output="logs/slurm-%j-%x.out"
 
 module load compiler/llvm
