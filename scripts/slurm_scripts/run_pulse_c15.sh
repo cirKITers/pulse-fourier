2f5c1ec40fbd8f2b99f954d122b07ddb82c8c2e1
TilliRS <pptry@mail.de>
2025-05-09 10:22:16 +0200

    Max mem for c15 and hea

--- scripts/slurm_scripts/run_pulse_c15.sh
+++ scripts/slurm_scripts/run_pulse_c15.sh
@@ -4,7 +4,7 @@
 #SBATCH --time=07:30:00
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=64
-#SBATCH --mem=300000MB
+#SBATCH --mem=380000MB
 #SBATCH --output="logs/slurm-%j-%x.out"
 
 module load compiler/llvm
