74e2e47af27c21719487488001ecd2346382bbee
TilliRS <pptry@mail.de>
2025-05-16 18:54:51 +0200

    Adjust mem HEA I

--- scripts/slurm_scripts/run_pulse_c15_I.sh
+++ scripts/slurm_scripts/run_pulse_c15_I.sh
@@ -4,7 +4,7 @@
 #SBATCH --time=48:00:00
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=64
-#SBATCH --mem=3200000MB
+#SBATCH --mem=320000MB
 #SBATCH --output="logs/slurm-%j-%x.out"
 
 module load compiler/llvm
