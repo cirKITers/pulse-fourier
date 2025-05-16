06da88a25308e8821fdcab912d6eb3116af742d6
TilliRS <pptry@mail.de>
2025-05-16 20:47:46 +0200

    run for seeds 51 to 5000

---
+++ scripts/slurm_scripts/run_pulse_c15_III.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#SBATCH --job-name=pulse_c15_parallel_seeds_2276_to_5000
+#SBATCH --partition=cpu
+#SBATCH --time=40:00:00
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=64
+#SBATCH --mem=320000MB
+#SBATCH --output="logs/slurm-%j-%x.out"
+
+module load compiler/llvm
+
+module load devel/python/3.11.7
+
+source /pfs/data6/home/ka/ka_scc/ka_tc6850/pulse-fourier/.venv/bin/activate
+
+
+start=2526
+end=5000
+
+for ((seed=start; seed<=end; seed++)); do
+  echo "Running with seed: $seed"
+  python /pfs/data6/home/ka/ka_scc/ka_tc6850/pulse-fourier/scripts/parallel_pulse_15.py "$seed"
+done
