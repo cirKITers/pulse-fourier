f7d5fa2342d252e23b1ec44eeb461200e8d21617
TilliRS <pptry@mail.de>
2025-05-16 19:33:05 +0200

    Remove TkAgg backend

--- src/pulse/pulse_backend.py
+++ src/pulse/pulse_backend.py
@@ -8,7 +8,7 @@ from visuals.bloch_sphere import bloch_sphere_trajectory
 # from visuals.bloch_sphere import bloch_sphere_trajectory, bloch_sphere_multiqubit_trajectory
 from visuals.probabilites import plot_populations
 
-matplotlib.use('TkAgg')
+# matplotlib.use('TkAgg')
 from pulse.operator import *
 
 from utils.definitions import *
